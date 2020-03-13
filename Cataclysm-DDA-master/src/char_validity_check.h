18dc766e5647baf1ce7aeaeefd24357131af7242
VincentBoyer23 <vincent.boyer2311@gmail.com>
2020-03-12 20:26:38 -0400

    change project

--- Cataclysm-DDA-master/src/char_validity_check.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef CHAR_VALIDITY_CHECK_H
-#define CHAR_VALIDITY_CHECK_H
-
-bool is_char_allowed( int ch );
-
-#endif
--- Cataclysm-DDA-master/src/get_version.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#ifndef GET_VERSION_H
-#define GET_VERSION_H
-const char *getVersionString();
-#endif
